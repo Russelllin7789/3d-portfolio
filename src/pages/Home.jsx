f6e134763460735bb04809f4e08a8ef895d8cb69
Russell Lin <superbr7789@gmail.com>
2023-11-28 21:04:32 +0800

    add rotation effect for sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
           />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}

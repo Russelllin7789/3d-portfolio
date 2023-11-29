1b237aca3476ad34b96709f009da693685553be4
Russell Lin <superbr7789@gmail.com>
2023-11-30 07:11:25 +0800

    add useAlert custom hook

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+// use custom hook to extract project's logic
+const useAlert = () => {
+  const [alert, setAlert] = useState({ show: false, text: "", type: "danger" });
+
+  const showAlert = ({ show = true, text, type = "danger" }) => {
+    setAlert({ show: true, text, type });
+  };
+  const hideAlert = () => {
+    setAlert({ show: false, text: "", type: "danger" });
+  };
+
+  return { alert, showAlert, hideAlert };
+};
+
+export default useAlert;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,12 +3,16 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
 import { Fox } from "../models/Fox";
 import Loader from "../components/Loader";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -40,6 +44,12 @@ const Contact = () => {
       .then(() => {
         setIsLoading(false);
 
+        showAlert({
+          show: true,
+          text: "Message sent successfully!",
+          type: "success",
+        });
+
         setTimeout(() => {
           setCurrentAnimation("idle");
           setForm({ name: "", email: "", message: "" });
@@ -49,11 +59,18 @@ const Contact = () => {
         setIsLoading(false);
         setCurrentAnimation("idle");
         console.log(err);
+
+        showAlert({
+          show: true,
+          text: "I didn't receive your message",
+          type: "danger",
+        });
       });
   };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form

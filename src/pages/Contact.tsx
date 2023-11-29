a72169ad590764601f322e1ef56a7cd2a7e35141
Russell Lin <superbr7789@gmail.com>
2023-11-29 20:32:55 +0800

    fulfill the email sending function via emailjs

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -14,6 +14,22 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Russell",
+          from_email: form.email,
+          to_email: "superbr7789@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+      });
   };
 
   return (

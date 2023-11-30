58a5c75329f4abcf47683f956ef2200bfaea5946
Russell Lin <superbr7789@gmail.com>
2023-11-30 21:14:54 +0800

    fulfill the alert function for contact form submission

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Russell
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software Engineer based in Taiwan, specializing in front-end UI
+          development through hands-on learning and building applications.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -53,6 +53,7 @@ const Contact = () => {
         setTimeout(() => {
           setCurrentAnimation("idle");
           setForm({ name: "", email: "", message: "" });
+          hideAlert();
         }, 3000);
       })
       .catch((err) => {
@@ -70,7 +71,7 @@ const Contact = () => {
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
-      {alert.show && <Alert />}
+      {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form

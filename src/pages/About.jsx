f253f355bcc335b4ebd4232f51b1db16ac108538
Russell Lin <superbr7789@gmail.com>
2023-11-30 22:13:30 +0800

    add skills section in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { skills } from "../constants";
 
 const About = () => {
   return (
@@ -12,8 +13,26 @@ const About = () => {
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
           Software Engineer based in Taiwan, specializing in front-end UI
-          development through hands-on learning and building applications.
+          development and e2e testing.
         </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );

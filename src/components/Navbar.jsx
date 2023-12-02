120288bb76301be15cc52039a31d90295e1f4893
Russell Lin <superbr7789@gmail.com>
2023-12-02 18:49:30 +0800

    correct some details like head title and favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
         to="/"
       >
-        <p className="blue-gradient_text">AH</p>
+        <p className="blue-gradient_text">RL</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
